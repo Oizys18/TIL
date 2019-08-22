43acdd14961f5614fb8b7d1e901477af64e592f8
Oizys18 <oizys18@gmail.com>
2019-08-22 18:32:25 +0900

    mod

--- SWEA/190818/SWEA5986.py
+++ SWEA/190818/SWEA5986.py
@@ -3,27 +3,31 @@
 # N = x + y + z 
 # import sys
 # sys.stdin = open("input.txt", 'r')
-sosuL = []
-temp = []
-for sosu in range(1,999):
-    for n in range(1,sosu):
-        if sosu % n != 0:
-            temp.append(sosu)
-        if sosu % n == 0: 
-            temp = []
-    sosuL += temp
-
-print(sosuL)
+def sosu(N):
+    if N < 2:
+        return False
+    # 소수 조건 넣기 
+    if N == 2:
+        return True
+    if N % 2 == 0:
+        return False
+    squareRootN = round(N ** 0.5) +1
+    for i in range(3,squareRootN,2):
+        if N % i == 0:
+            return False
+    return True
 
 # for T in range(int(input())):
 #     N = int(input())
-#     oddL = [i for i in range(1,N,2)]
-#     print(oddL)
-    # n = len(oddL)
-    # for j in range(1<<n):
-    #     for k in range(n+1):
-    #         if j & (1<<k):
+#     sosuL = [x for x in range(1, N+1) if sosu(x)]
+#     temp = []
+#     cnt = 0
+#     for i in range(len(sosuL)):
+#         for j in range(i,len(sosuL)):
+#             for k in range(j,len(sosuL)):
+#                 if sosuL[i]+ sosuL[j] + sosuL[k] == N:
+#                     cnt += 1 
+#     print("#{0} {1}".format(T+1,cnt))
 
-    #             print(oddL[k], end=", ")
-    #     print()
-    # print()    
+    
+print([x for x in range(1, 50000) if sosu(x)])
